79f3244120181085590ab15f6afc1f1b9637ee4b
Lucas S <lucas.santos.silva2011@gmail.com>
2023-01-30 19:16:39 -0300

    Setup JavaFX

--- src/main/java/com/lslm/Main.java
+++ src/main/java/com/lslm/Main.java
@@ -2,13 +2,26 @@ package com.lslm;
 
 import com.lslm.repositories.Configuration;
 import com.lslm.ui.MainMenu;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class Main {
+public class Main extends Application {
     public static void main(String[] args) throws SQLException {
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.show();
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        HBox hBox = new HBox(new Text("Olá"));
+
+        Scene scene = new Scene(hBox, 800, 600);
+        stage.setScene(scene);
+        stage.show();
     }
 }
